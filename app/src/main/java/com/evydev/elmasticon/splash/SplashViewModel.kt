5db2ceae2a1f71effb85b1c23ae846927b5d9294
elvyn <elvyn.paucar.ponce@gmail.com>
2026-01-31 21:47:23 -0500

    feat: complete splash loading flow

--- app/src/main/java/com/evydev/elmasticon/splash/SplashViewModel.kt
+++ app/src/main/java/com/evydev/elmasticon/splash/SplashViewModel.kt
@@ -1,4 +1,33 @@
 package com.evydev.elmasticon.splash
 
-class SplashViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class SplashViewModel() : ViewModel() {
+
+    private val _state = MutableStateFlow<SplashUiState>(SplashUiState.Loading)
+    val state : StateFlow<SplashUiState> = _state
+
+    init {
+        startSplash()
+    }
+
+    private fun startSplash() {
+
+        viewModelScope.launch {
+
+            try {
+                delay(2000)
+                _state.value = SplashUiState.Success
+
+            }catch (e: Exception){
+                _state.value = SplashUiState.Error("Error al cargar...")
+            }
+
+        }
+    }
 }
